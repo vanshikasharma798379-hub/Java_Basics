8429dfbb0d14e73847481689a40650936a5d6b64
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-06-23 20:11:17 +0530

    Added X Pattern program

---
+++ Patterns/X_Pattern.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Xpattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number of rows");
+        int n = sc.nextInt();
+        for( int i = 1; i<=n; i++)
+        {
+            for( int j = 1; j<=n; j++)
+            {
+                if(i==j || i+j==(n+1))
+                {
+                    System.out.print("*"+"");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+             System.out.println();
+        }
+        sc.close();
+
+    }
+        
+    }
+    
