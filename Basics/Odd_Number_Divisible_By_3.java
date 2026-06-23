3904f3b8e3f385c59878465a4338a67c5ce10d07
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-06-23 17:24:59 +0530

    Added Odd Number Divisible By 3 program

---
+++ Basics/Odd_Number_Divisible_By_3.java
@@ -0,0 +1,14 @@
+public class Oddnumberdivisibleby3 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=100; i++)
+        {
+            if(i % 2 != 0)
+            {
+                if(i%3==0)
+                {
+                    System.out.print(i+" ");
+                }
+            }
+        }
+    }   
+}
