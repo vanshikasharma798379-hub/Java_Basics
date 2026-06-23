2aa6abf07a4ba50b644208b370cbb3dcba04d167
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-06-23 20:12:47 +0530

    Added Floyd's Triangle Pattern program

---
+++ Patterns/Floyds_Triangle_Pattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Floydstriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number of rows");
+        int n = sc.nextInt();
+        int a =1;
+        for( int i = 1; i<=n; i++)
+        {
+            for( int j = 1; j<=i; j++)
+            {
+                System.out.print(a+" ");
+                a++;
+            }
+             System.out.println( );
+        }
+        sc.close();
+
+    }
+        
+    
+}
