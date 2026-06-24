81fa2059c5470a4fa48aed32923c8c3c2c87f349
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-06-24 11:03:24 +0530

    Added Maximum Element In Array program

---
+++ Arrays/Maximum_Element_In_Array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Maxelementofarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number");
+        int[] arr = new int[50];
+        int n = sc.nextInt();
+        for( int i = 0; i<n; i++)
+        {
+             arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for( int i = 1; i < n; i++)
+        {
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.print(max);
+        sc.close();
+    }
+    
+}
