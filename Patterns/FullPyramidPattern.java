7673f72603f241046857dbeec5cf03ab3fdfbd26
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-07-11 21:02:59 +0530

    Added Full Pyramid Pattern

---
+++ Patterns/FullPyramidPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tpattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number of rows");
+        int n = sc.nextInt();
+        for( int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=n-i; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int k = 1; k<=(2*i-1); k++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println(  );
+        }
+        sc.close();
+    }
+    
+}
+
