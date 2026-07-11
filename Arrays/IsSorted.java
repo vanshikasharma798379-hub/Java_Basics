e10a5004204c565d9b896a688a1bc0230b718204
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-07-11 18:14:09 +0530

    Added Is Sorted array solution

---
+++ Arrays/IsSorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        // code here
+        int n = arr.length;
+        for(int i = 0; i<n-1; i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
