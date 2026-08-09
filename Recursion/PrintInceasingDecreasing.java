dc3d86b24b7a0d6b22275aa4aac88ba1fd6ac304
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-08-09 14:12:11 +0530

    Create PrintInceasingDecreasing.java

---
+++ Recursion/PrintInceasingDecreasing.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PrintIncreasingDereasing {
+    public static void Print(int n)
+    {
+        if(n==1)
+        {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        Print(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Print(n);
+        sc.close();
+    }  
+    
+}
